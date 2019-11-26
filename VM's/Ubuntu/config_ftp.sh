9a9bc546ed192b62a7b013e14160424b5b226a38
jonathanwiggli <jonathanwiggli@bluewin.ch>
2019-11-26 16:29:50 +0100

    Update

--- VM's/Ubuntu/config_ftp.sh
+++ VM's/Ubuntu/config_ftp.sh
@@ -1,4 +1,5 @@
 sudo apt-get update 
 sudo apt-get install vsftpd -y
+sudo useradd ftp-user -d /home/FTP -m 
 echo 'ftp-user:user' | sudo chpasswd
 sudo sed -i 's/XKBLAYOUT="us"/XKBLAYOUT="ch"/g' /etc/default/locale
