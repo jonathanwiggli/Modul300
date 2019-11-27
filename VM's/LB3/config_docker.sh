2edc26450fb17115f5cf9262e4627b779c8957b1
jonathanwiggli <jonathanwiggli@bluewin.ch>
2019-11-27 13:32:27 +0100

    LB3 Final

--- VM's/LB3/config_docker.sh
+++ VM's/LB3/config_docker.sh
@@ -3,4 +3,7 @@ sudo sudo apt install docker.io -y
 cd /vagrant/apache
 sudo docker build -t apache .
 sudo docker run --rm -d -p 8080:80 -v `pwd`/web:/var/www/html --name apache apache
+cd /vagrant/ftp
+sudo docker build -t ftp .
+sudo docker run --rm -d -p 20:20 --name ftp ftp
 sudo sed -i 's/XKBLAYOUT="us"/XKBLAYOUT="ch"/g' /etc/default/locale
