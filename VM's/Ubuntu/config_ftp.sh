4ee3bb5c9b0668474fe4a4f278ba7462163bcf84
jonathanwiggli <jonathanwiggli@bluewin.ch>
2019-11-27 09:32:26 +0100

    LB2 Final

--- VM's/Ubuntu/config_ftp.sh
+++ VM's/Ubuntu/config_ftp.sh
@@ -1,5 +1,8 @@
 sudo apt-get update 
 sudo apt-get install vsftpd -y
-sudo useradd ftp-user -d /home/FTP -m 
-echo 'ftp-user:user' | sudo chpasswd
+sudo cp /vagrant/vsftpd.conf /etc
+sudo service vsftpd restart
 sudo sed -i 's/XKBLAYOUT="us"/XKBLAYOUT="ch"/g' /etc/default/locale
+sudo ufw allow ssh
+sudo ufw allow ftp
+sudo ufw --force enable
--- VM's/Ubuntu/config_web.sh
+++ VM's/Ubuntu/config_web.sh
@@ -1,3 +1,7 @@
-apt-get update
-apt-get install apache2 -y
+sudo apt-get update
+sudo apt-get install apache2 -y
 sudo sed -i 's/XKBLAYOUT="us"/XKBLAYOUT="ch"/g' /etc/default/locale
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow https
+sudo ufw --force enable
