af8fb51d88850caea4ebeb998478721ad971ed95
jonathanwiggli <jonathanwiggli@bluewin.ch>
2019-12-10 21:44:09 +0100

    Update Docker & Vagrantfile

--- VM's/LB3/config_docker.sh
+++ VM's/LB3/config_docker.sh
@@ -5,5 +5,5 @@ sudo docker build -t apache .
 sudo docker run --rm -d -p 8080:80 -v `pwd`/web:/var/www/html --name apache apache
 cd /vagrant/ftp
 sudo docker build -t ftp .
-sudo docker run --rm -d -p 20:20 --name ftp ftp
+sudo docker run --rm -d -p 1020:20 -v /conf:/etc/vsftpd --name ftp ftp
 sudo sed -i 's/XKBLAYOUT="us"/XKBLAYOUT="ch"/g' /etc/default/locale
